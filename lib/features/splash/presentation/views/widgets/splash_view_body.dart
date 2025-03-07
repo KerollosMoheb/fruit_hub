03bcbf8f81586a2e6455e8432d9fdf77a68450cf
kerollosmoheb <kokomoheb73@gmail.com>
2025-03-07 17:25:28 +0200

    splash view logic done

---
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:fruits_e_commerce_app/core/utils/app_images.dart';
+import 'package:fruits_e_commerce_app/features/on_boarding/presentation/views/on_boarding_view.dart';
+
+class SplashViewBody extends StatefulWidget {
+  const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody> {
+  @override
+  void initState() {
+    super.initState();
+    excuteNavigation();
+  }
+
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [SvgPicture.asset(Assets.imagesPlant)],
+        ),
+        SvgPicture.asset(Assets.imagesLogo),
+        SvgPicture.asset(Assets.imagesSplashBottom, fit: BoxFit.fill),
+      ],
+    );
+  }
+
+  void excuteNavigation() {
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacementNamed(context, OnBoardingView.routeName);
+    });
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:fruits_e_commerce_app/core/helper_functions/on_generate_routes.dart';
+import 'package:fruits_e_commerce_app/features/splash/presentation/views/splash_view.dart';
+
+void main() {
+  runApp(const FruitHub());
+}
+
+class FruitHub extends StatelessWidget {
+  const FruitHub({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      onGenerateRoute: onGenerateRoutes,
+      initialRoute: SplashView.routeName,
+    );
+  }
+}
